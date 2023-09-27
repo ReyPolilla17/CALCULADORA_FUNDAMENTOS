3ccaa6d9bddf5b0271e6e44df53ca14e907185dc
Luis Julián Zamora Treviño <naway.luisju@gmail.com>
2023-09-26 19:37:16 -0600

    Sumar 10 numeros agregado: al introducir a, se suman 10 numeros dados por el usuario

--- calculadora.c
+++ calculadora.c
@@ -6,10 +6,15 @@ Entradas: un caracter
 Salidas: su función
 */
 
+void leer_arreglo(int arr[]);
+int sumar_arreglo(int arr[]);
+
 int main(void)
 {
 // definir operar
   char operar = 0;
+  int res = 0;
+  int nums[10];
 
 // guardar el valor del usuario en operar
   printf("Introduce un operador matematico: ");
@@ -38,6 +43,20 @@ int main(void)
       printf("Division\n");
       break;
     }
+    // si se escoje a, se sumaran 10 numeros usando arreglos
+    case 'a':
+    {
+      // guardar 1 numeros
+      printf("Introduce 10 numeros:\n");
+      
+      leer_arreglo(nums);
+      // res será igual a la suma de los 10 numeros
+      res = sumar_arreglo(nums);
+
+      printf("El resultado de la suma es %d\n", res);
+
+      break;
+    }
 // si no está la acción, regresa error
     default:
     {
@@ -47,4 +66,47 @@ int main(void)
   }
 
   return 0;
+}
+
+/*
+Esta función leerá 10 numeros y los guardará en un arreglo
+Entradas: 10 numeros
+Salidas: los 10 numeros guardados en el arreglo
+*/
+void leer_arreglo(int arr[])
+{
+  // contador
+  int i = 0;
+
+  // repetir 10 veces
+  for(i = 0; i < 10; i++)
+    {
+      // pedir un numero y guardarlo en la siguiente posición
+      printf("Introduce un numero: ");
+      scanf("%d", &arr[i]);
+    }
+
+  return;
+}
+
+/*
+Esta función suma los 10 numeros dentro de un arreglo
+Entradas: 1 arreglo con 10 numeros
+Salidas: la suma de los 10 numeros del arreglo
+*/
+int sumar_arreglo(int arr[])
+{
+  // contador y variable del resultado
+  int i = 0;
+  int res = 0;
+
+  // repetir 10 veces
+  for(i = 0; i < 10; i++)
+    {
+      // sumarle a res el valor del arreglo actual
+      res = res + arr[i];
+    }
+
+  // reglresar res
+  return res;
 }
